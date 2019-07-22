e5ab0fbf6533125c7675cb76ac93fe1aecd55157
Your Name <you@example.com>
2019-07-22 09:46:03 +0530

    initialcommit

---
+++ InsertUpdateGridView/Default.aspx
@@ -0,0 +1,112 @@
+﻿ <%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="InsertUpdateGridView.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 37%;
+            height: 120px;
+        }
+        .auto-style2 {
+            height: 51px;
+        }
+        .auto-style3 {
+            height: 51px;
+            width: 138px;
+        }
+        .auto-style4 {
+            width: 138px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style3">Up Load File</td>
+                    <td class="auto-style2">
+                        <asp:FileUpload ID="FileUpload1" runat="server" Height="41px" />
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">&nbsp;</td>
+                    <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <asp:Button ID="btnUpload" runat="server" OnClick="btnUpload_Click" Text="Upload to DB" />
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:GridView ID="gvDetails" runat="server" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="3" AutoGenerateColumns="false" Height="252px" Width="481px" ShowFooter="true" DataKeyNames="Id" ShowHeaderWhenEmpty="true" OnRowEditing="gvDetails_RowEditing" OnRowCancelingEdit="gvDetails_RowCancelingEdit" OnRowUpdating="gvDetails_RowUpdating" OnRowDeleting="gvDetails_RowDeleting">
+                <FooterStyle BackColor="White" ForeColor="#000066" />
+                <HeaderStyle BackColor="#006699" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="White" ForeColor="#000066" HorizontalAlign="Left" />
+                <RowStyle ForeColor="#000066" />
+                <SelectedRowStyle BackColor="#669999" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#007DBB" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#00547E" />
+
+                <Columns>
+                    <asp:TemplateField HeaderText="Name">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Name") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtName" Text='<%# Eval("Name") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtName" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="City">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("City") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtCity" Text='<%# Eval("City") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtCity" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Country">
+                        <ItemTemplate>
+                            <asp:Label Text='<%# Eval("Country") %>' runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:TextBox ID="txtCountry" Text='<%# Eval("Country") %>' runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:TextBox ID="txtCountry" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:ImageButton ImageUrl="~/images/edit_button.PNG" CommandName="Edit" ToolTip="Edit" Width="20px" Height="20px" runat="server" />
+                            <asp:ImageButton ImageUrl="~/images/delete.PNG" CommandName="Delete" ToolTip="Delete" Width="20px" Height="20px" runat="server" />
+                        </ItemTemplate>
+                        <EditItemTemplate>
+                            <asp:ImageButton ImageUrl="~/images/save.PNG" CommandName="Update" ToolTip="Update" Width="20px" Height="20px" runat="server" />
+                            <asp:ImageButton ImageUrl="~/images/cancel.PNG" CommandName="Cancel" ToolTip="Cancel" Width="20px" Height="20px" runat="server" />
+                        </EditItemTemplate>
+                        <FooterTemplate>
+                            <asp:ImageButton ImageUrl="~/images/add.PNG" CommandName="AddNew" ToolTip="AddNew" Width="20px" Height="20px" runat="server" />
+                        </FooterTemplate>
+                    </asp:TemplateField>
+                </Columns>
+            </asp:GridView>
+            <br />
+            <asp:Label ID="lblsuccessmsg" Text="" runat="server" ForeColor="Green" />
+            <br />
+            <asp:Label ID="lblErrormsg" Text="" runat="server" ForeColor="Red" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ InsertUpdateGridView/Default.aspx.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Data.OleDb;
+
+namespace InsertUpdateGridView
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string connectionstring = @"Data Source=DEEPU\SQLEXPRESS01; Integrated Security=true; Initial Catalog=Demo";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateGridView();
+            }
+        }
+
+        private void PopulateGridView()
+        {
+            DataTable dtb1 = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                con.Open();
+                SqlDataAdapter sda = new SqlDataAdapter("Select * from empdetails", con);
+            
+                sda.Fill(dtb1);
+            }
+            if (dtb1.Rows.Count > 0)
+            {
+                gvDetails.DataSource = dtb1;
+                gvDetails.DataBind();
+            }
+            else
+            {
+                dtb1.Rows.Add(dtb1.NewRow());
+                gvDetails.DataSource = dtb1;
+                gvDetails.DataBind();
+                gvDetails.Rows[0].Cells.Clear();
+                gvDetails.Rows[0].Cells.Add(new TableCell());
+                gvDetails.Rows[0].Cells[0].ColumnSpan = dtb1.Columns.Count;
+                gvDetails.Rows[0].Cells[0].Text = "No Data Found";
+                gvDetails.Rows[0].Cells[0].HorizontalAlign = HorizontalAlign.Center;
+            }
+        }
+
+        protected void gvDetails_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            gvDetails.EditIndex = e.NewEditIndex;
+            PopulateGridView();
+        }
+
+        protected void gvDetails_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            gvDetails.EditIndex = -1;
+            PopulateGridView();
+        }
+
+        protected void gvDetails_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionstring))
+                {
+                    con.Open();
+                    string query = "Update empdetails set Name=@Name, City=@City, Country=@Country where Id = @Id";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Name", (gvDetails.Rows[e.RowIndex].FindControl("txtName") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@City", (gvDetails.Rows[e.RowIndex].FindControl("txtCity") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@Country", (gvDetails.Rows[e.RowIndex].FindControl("txtCountry") as TextBox).Text.Trim());
+                    cmd.Parameters.AddWithValue("@Id", Convert.ToInt32(gvDetails.DataKeys[e.RowIndex].Value.ToString()));
+                    cmd.ExecuteNonQuery();
+                    gvDetails.EditIndex = -1;
+                    PopulateGridView();
+                    lblsuccessmsg.Text = "Updated Successfully";
+                }
+            }
+            catch (SqlException ex)
+            {
+                lblErrormsg.Text = ex.Message;
+            }
+        }
+
+        protected void gvDetails_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionstring))
+                {
+                    con.Open();
+                    string query = "Delete from empdetails where Id = @Id";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    
+                    cmd.Parameters.AddWithValue("@Id", Convert.ToInt32(gvDetails.DataKeys[e.RowIndex].Value.ToString()));
+                    cmd.ExecuteNonQuery();
+                    gvDetails.EditIndex = -1;
+                    PopulateGridView();
+                    lblsuccessmsg.Text = "Deleted Successfully";
+                }
+            }
+            catch (SqlException ex)
+            {
+                lblErrormsg.Text = ex.Message;
+            }
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            String Name;
+            String City;
+            String Country;
+            string path = System.IO.Path.GetFileName(FileUpload1.FileName);
+            path = path.Replace(" ", "");
+            FileUpload1.SaveAs(Server.MapPath("~/ExcelFile/") + path);
+            String ExcelPath = Server.MapPath("~/ExcelFile/") + path;
+            OleDbConnection mycon = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + ExcelPath + "; Extended Properties=Excel 8.0; Persist Security Info = False");
+            mycon.Open();
+            OleDbCommand cmd = new OleDbCommand("select * from [Sheet2$]", mycon);
+            OleDbDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                // Response.Write("<br/>"+dr[0].ToString());
+                
+                Name = dr[0].ToString();
+                City = dr[1].ToString();
+                Country = dr[2].ToString();
+                savedata(Name, City, Country);
+
+
+            }
+            Label1.Text = "Data Has Been Saved Successfully";
+        }
+
+        private void savedata(String Name1, String City1, String Country1)
+        {
+            String query = "insert into empdetails(Name,City,Country) values('" + Name1 + "','" + City1 + "','" + Country1 + "')";
+            
+            SqlConnection con = new SqlConnection(connectionstring);
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = query;
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
